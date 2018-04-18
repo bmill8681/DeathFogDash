ed3fedf4b704de3bc92196f1da644d2b46f71bc1
August Yadon <AugustYadon@gmail.com>
2018-04-18 16:10:08 -0400

    Added some custom event scripts for testing.
    I wrote these for plunker, they're purely to speed up testing when you're trying to debug something. You just add the UnityFunctionCaller.cs script to your object and you will be able to instantly turn any public function into an editor button.

---
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/UtilityFunctionCaller.cs
@@ -0,0 +1,24 @@
+﻿
+using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_EDITOR 
+
+
+[ExecuteInEditMode]
+public class UtilityFunctionCaller : MonoBehaviour
+{
+    [System.Serializable]
+    public struct AuggoVent {
+        public string Name;
+        public UnityEvent unity_event;
+    }
+    [Header("Custom Events")]
+    public AuggoVent[] things;
+    [Space(20)]
+    [Header("Buttons")]
+    public bool allow_actions;
+
+
+}
+#endif
---
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/UtilityFunctionCallerEditor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR 
+using UnityEditor;
+
+[CustomEditor(typeof(UtilityFunctionCaller))]
+public class UtilityFunctionCallerEditor : Editor {
+
+    public UtilityFunctionCaller myCaller;
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        myCaller = (UtilityFunctionCaller)target;
+        if (myCaller.allow_actions && myCaller.things != null)
+        {
+            int L = myCaller.things.Length;
+
+            for (int i = 0; i < L; i++)
+            {
+                if (myCaller.things[i].Name != "")
+                {
+                    if (GUILayout.Button(myCaller.things[i].Name))
+                    {
+                        myCaller.things[i].unity_event.Invoke();
+                    }
+                }
+                else if (GUILayout.Button("Button " + i))
+                {
+                    myCaller.things[i].unity_event.Invoke();
+                }
+            }
+        }
+        
+    }
+}
+#endif
