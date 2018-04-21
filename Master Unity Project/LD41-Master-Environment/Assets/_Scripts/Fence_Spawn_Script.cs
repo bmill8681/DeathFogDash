418fc3e98ea8f4a0493ffb10e7e21b9732898838
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-21 15:11:53 -0600

    Park Fixed

--- Master Unity Project/LD41-Master-Environment/Assets/_Scripts/Fence_Spawn_Script.cs
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/Fence_Spawn_Script.cs
@@ -5,13 +5,15 @@ using UnityEngine;
 public class Fence_Spawn_Script : MonoBehaviour {
 
     public GameObject[] fences = new GameObject[3];
+    public Transform parkParent;
 
     private void Start()
     {
         int objNum = Random.Range(-1, fences.Length);
         if (objNum != -1)
         {
-            GameObject newCan = Instantiate(fences[objNum], transform.position, transform.rotation);
+            GameObject newFence = Instantiate(fences[objNum], transform.position, transform.rotation);
+            newFence.transform.SetParent(parkParent);
         }
         Destroy(this.gameObject);
     }
