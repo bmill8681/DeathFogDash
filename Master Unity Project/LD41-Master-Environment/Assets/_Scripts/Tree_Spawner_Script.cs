b79515be0dca685d71002a7c22bf33e42524646f
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-21 17:40:36 -0600

    Fixed the park

--- Master Unity Project/LD41-Master-Environment/Assets/_Scripts/Tree_Spawner_Script.cs
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/Tree_Spawner_Script.cs
@@ -6,14 +6,17 @@ public class Tree_Spawner_Script : MonoBehaviour {
 
     public GameObject[] trees = new GameObject[3];
     public Transform parkParent;
+    
 
     private void Start()
     {
+        float scaleFactor = Random.Range(30.0f, 50.0f);
         int objNum = Random.Range(-1, trees.Length);
         if (objNum != -1)
         {
             GameObject newTree = Instantiate(trees[objNum], transform.position, transform.rotation);
             newTree.transform.SetParent(parkParent);
+            newTree.transform.localScale = new Vector3(scaleFactor * parkParent.localScale.x, scaleFactor * parkParent.localScale.y, scaleFactor * parkParent.localScale.z);
         }
         Destroy(this.gameObject);
     }
