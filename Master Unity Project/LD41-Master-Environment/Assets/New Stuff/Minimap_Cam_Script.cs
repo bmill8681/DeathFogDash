fee425b9b38b7aef1bf7831beb478bef4299b002
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-21 11:13:39 -0600

    Minimap, Camera, Inventory GUI

---
+++ Master Unity Project/LD41-Master-Environment/Assets/New Stuff/Minimap_Cam_Script.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap_Cam_Script : MonoBehaviour {
+
+    public Transform playerPos;
+    public static float cam_height = 10.0f;
+    private float zoomDistance = 6.5f;
+    private Camera miniCam;
+    private void Awake()
+    {
+        miniCam = this.GetComponent<Camera>();
+        miniCam.orthographic = true;
+        setMiniCamZoom();
+    }
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3(playerPos.position.x, 10.0f, playerPos.position.z);
+        
+	}
+
+    public void setMiniCamZoom(float zoom) // Use this to set minimap camera zoom level from settings menu
+    {
+        miniCam.orthographicSize = zoom;
+    }
+
+    public void setMiniCamZoom()
+    {
+        miniCam.orthographicSize = zoomDistance;
+    }
+}
---
+++ Master Unity Project/LD41-Master-Environment/Assets/New Stuff/Tile_Selection_Script.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile_Selection_Script : MonoBehaviour {
+
+    public Image[] tileBorders = new Image[7];
+    public static int curTile = 0;
+
+    public int getCurTile()
+    {
+        return curTile;
+    }
+
+    private void Awake()
+    {
+        for(int x = 1; x < tileBorders.Length; x++)
+        {
+            Image curBorder = tileBorders[x];
+            curBorder.enabled = false;
+        }
+    }
+
+    private void Update()
+    {
+        highlightTile();
+    }
+
+    private void highlightTile()
+    {
+        scrollTileHighlight();
+        keyTileHighlight();
+    }
+
+    private void keyTileHighlight() // Highlight tile to use using the alphanumeric number keys 1-7
+    {
+        int selectedTile = curTile;
+
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            selectedTile = 0;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            selectedTile = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            selectedTile = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            selectedTile = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            selectedTile = 4;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            selectedTile = 5;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            selectedTile = 6;
+        }
+
+        if(selectedTile != curTile)
+        {
+            Image curBoarder = tileBorders[curTile];
+            curBoarder.enabled = false;
+            curTile = selectedTile;
+            curBoarder = tileBorders[curTile];
+            curBoarder.enabled = true;
+        }
+        
+
+    }
+
+    private void scrollTileHighlight()
+    {
+        double scrollDirection = Input.GetAxis("Mouse ScrollWheel");
+
+        if (scrollDirection > 0)
+        {
+            Image curBoarder = tileBorders[curTile];
+            curBoarder.enabled = false;
+            curTile++;
+            if (curTile > 6)
+            {
+                curTile = 0;
+            }
+            curBoarder = tileBorders[curTile];
+            curBoarder.enabled = true;
+        }
+        else if (scrollDirection < 0)
+        {
+            Image curBoarder = tileBorders[curTile];
+            curBoarder.enabled = false;
+            curTile--;
+            if (curTile < 0)
+            {
+                curTile = 6;
+            }
+            curBoarder = tileBorders[curTile];
+            curBoarder.enabled = true;
+        }
+    } //Highlights appropriate tile using Mouse 3 Scrolling
+}
