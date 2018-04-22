76e9fe6f36691e7090887eb25c951e7b826a5661
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-21 18:51:57 -0600

    Added colliders and transparent shaders to 1x2 buildings, added letter W texture, added ? texture

---
+++ Master Unity Project/LD41-Master-Environment/Assets/Slab_Prefabs/Item Spawners/Single_Tree_Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Single_Tree_Spawner : MonoBehaviour
+{
+
+    public GameObject[] trees = new GameObject[3];
+
+
+    private void Start()
+    {
+        float scaleFactor = Random.Range(30.0f, 50.0f);
+        int objNum = Random.Range(-1, trees.Length);
+        if (objNum != -1)
+        {
+            GameObject newTree = Instantiate(trees[objNum], transform.position, transform.rotation);
+            newTree.transform.localScale = new Vector3(scaleFactor * transform.localScale.x, scaleFactor * transform.localScale.y, scaleFactor * transform.localScale.z);
+        }
+        Destroy(this.gameObject);
+    }
+}
