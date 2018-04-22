f28f45abfd0e89973fa44c6bcbb383d4e44f37ab
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-22 11:20:09 -0600

    Fixed Char Model, Tile SFX added

---
+++ Master Unity Project/LD41-Master-Environment/Assets/SFX/DestroyAfterPlaying.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterPlaying : MonoBehaviour {
+
+    private AudioSource source;
+	// Use this for initialization
+	void Awake () {
+        source = GetComponent<AudioSource>();
+	}
+
+    private void Start()
+    {
+        source.Play();
+    }
+    // Update is called once per frame
+    void LateUpdate () {
+		if(!source.isPlaying)
+        {
+            Destroy(this.gameObject);
+        }
+	}
+}
