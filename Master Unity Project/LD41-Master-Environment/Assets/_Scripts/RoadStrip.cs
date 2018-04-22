62a43e18228a01d9ce35df34939b483b4d529ffb
August Yadon <AugustYadon@gmail.com>
2018-04-21 20:14:22 -0400

    New code for inventory

--- Master Unity Project/LD41-Master-Environment/Assets/_Scripts/RoadStrip.cs
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/RoadStrip.cs
@@ -24,13 +24,24 @@ public class RoadStrip : MonoBehaviour {
         MeshRenderer mr;
         for(int i = 0; i < word.Length; i++)
         {
-            
-            Transform letter_holder = Instantiate(WordHandler.instance.LetterPrefab, slots[i + offset].position, transform.rotation).transform;
-            mr = letter_holder.GetComponent<MeshRenderer>();
-            mr.material.SetTexture("_MainTex",WordHandler.instance.lettertile_textures[(int)word[i]]);
-            letter_holder.parent = slots[i + offset].parent;
-            slots[i + offset].gameObject.SetActive(false);
-            items[i + offset] = letter_holder.GetComponent<Item>();
+            if (word[i] == Tile.question_mark || word[i] == Tile._)
+            {
+                Transform letter_holder = Instantiate(WordHandler.instance.LetterPrefab, slots[i + offset].position, transform.rotation).transform;
+                mr = letter_holder.GetComponent<MeshRenderer>();
+                mr.material.SetTexture("_MainTex", WordHandler.instance.lettertile_textures[(int)word[i]]);
+                letter_holder.parent = slots[i + offset].parent;
+                slots[i + offset].gameObject.SetActive(false);
+                items[i + offset] = letter_holder.GetComponent<Item>();
+            }
+            else
+            {
+                Transform letter_holder = Instantiate(WordHandler.instance.LetterPrefab, slots[i + offset].position, transform.rotation).transform;
+                mr = letter_holder.GetComponent<MeshRenderer>();
+                mr.material.SetTexture("_MainTex", WordHandler.instance.lettertile_textures[(int)word[i]]);
+                letter_holder.parent = slots[i + offset].parent;
+                slots[i + offset].gameObject.SetActive(false);
+                items[i + offset] = letter_holder.GetComponent<Item>();
+            }
         }
 
     }
