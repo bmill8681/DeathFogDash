199de14e4b0bf0536f6efa889b3561526321cb89
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-21 22:14:39 -0600

    DeathRay Added

---
+++ Master Unity Project/LD41-Master-Environment/Assets/Slab_Prefabs/Death Ray/DeathSound_Script.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathSound_Script : MonoBehaviour {
+
+    private AudioSource aSource;
+    public AudioClip[] destroySounds = new AudioClip[3];
+
+    private void Awake()
+    {
+        aSource = GetComponent<AudioSource>();
+        playDeathSound();
+    }
+
+    private void LateUpdate()
+    {
+        if(!aSource.isPlaying)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void playDeathSound()
+    {
+        int sound = Random.Range(0, destroySounds.Length);
+        AudioClip sClip = destroySounds[sound];
+        aSource.clip = sClip;
+        aSource.Play();
+    }
+}
---
+++ Master Unity Project/LD41-Master-Environment/Assets/Slab_Prefabs/Death Ray/Destroyer_Script.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer_Script : MonoBehaviour {
+
+    public GameObject dSoundObj;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag != "persistent")
+        {
+            int chance = Random.Range(0, 5);  // Chance of playing a sound when destroyed is 60%
+            if(chance == 3)
+                Instantiate(dSoundObj, other.transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+        }
+    }
+
+
+}
