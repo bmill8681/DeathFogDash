0b4e2b73cc076db28c368bc0359fd0f6705f5409
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-23 11:02:31 -0600

    Fog Distance Indicator, Main menu Update

---
+++ Master Unity Project/LD41-Master-Environment/Assets/_Scripts/FogDistanceIndicator_Script.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FogDistanceIndicator_Script : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject fog;
+    public Image fogUI;
+
+
+    private float MAX_HEIGHT = 195.0f;
+    private float MIN_HEIGHT = 0.0f;
+    private float MAX_SPREAD = 175.0f;
+
+    private void Awake()
+    {
+        fogUI.rectTransform.localPosition = new Vector3(0f, 0f, 0f);
+    }
+    void LateUpdate () {
+        float pos = getDistance();
+        fogUI.rectTransform.localPosition = new Vector3(-5.0f, -pos + 100.0f, 0.0f);
+    }
+
+    private float getDistance()
+    {
+        float fogPos = fog.transform.position.x;
+        float pPos = player.transform.position.x;
+        float spread = pPos - fogPos;
+        if (spread > MAX_SPREAD)
+        {
+            spread = MAX_SPREAD;
+        }
+        else if (spread < 0.0f)
+        {
+            spread = 0.0f;
+        }
+        return spread;
+    }
+}
