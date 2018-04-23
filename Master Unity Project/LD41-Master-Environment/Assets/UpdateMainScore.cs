e5b55beba1909121f1b6f92caeddd9fb31418a8f
LLimb <38405413+LLimb@users.noreply.github.com>
2018-04-23 15:30:30 -0600

    HighScore in game

---
+++ Master Unity Project/LD41-Master-Environment/Assets/UpdateMainScore.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UpdateMainScore : MonoBehaviour {
+
+    public TextMeshProUGUI scoreText;
+	
+	// Update is called once per frame
+	void Update () {
+        int score = GameController.instance.total_points;
+        scoreText.text = score.ToString();
+	}
+}
